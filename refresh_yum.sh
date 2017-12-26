0f093d6018311dec6167a05ce938f215c18d3a91
Sunil Boga <30490515+boga5@users.noreply.github.com>
2017-12-26 15:15:25 +0530

    Update refresh_yum.sh

--- refresh_yum.sh
+++ refresh_yum.sh
@@ -2,4 +2,4 @@
 
 sudo createrepo --update $1
 #createrepo --update <repo_path>
-# $1 refers to the path of YUM repo
+# $1 refers to the path of YUM repo. It must be passed as an argument to the script.
